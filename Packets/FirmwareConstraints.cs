e50d509965b43fc1ce2026cc5b674b017f816ed1
qrp73 <46676744+qrp73@users.noreply.github.com>
2024-09-26 13:23:53 +0300

    extend supported eeprom address space to 64kB, issue #5

--- Packets/FirmwareConstraints.cs
+++ Packets/FirmwareConstraints.cs
@@ -3,7 +3,7 @@
     public static class FirmwareConstraints
     {
         public static int MinEepromAddr = 0x0000;
-        public static int MaxEepromAddr = 0x2000-1;
+        public static int MaxEepromAddr = 0x10000-1;
         public static int MinFlashAddr  = 0x0000;
         public static int MaxFlashAddr  = 0xf000-1; // tested with egzumer/1o11_fagci_spectrum_v0.6_packed.bin (size=0xeff4, offsetFinal=0xf000)
     }
